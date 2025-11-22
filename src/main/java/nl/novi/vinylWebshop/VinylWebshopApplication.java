ee5407b886825acf867f7a1d4fba5516ddc131c2
JappieSoft <j.aeromaint@gmail.com>
2025-11-22 14:42:23 +0100

    first commit

---
+++ src/main/java/nl/novi/vinylWebshop/VinylWebshopApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.vinylWebshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylWebshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylWebshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/novi/vinylWebshop/VinylWebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.vinylWebshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylWebshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
